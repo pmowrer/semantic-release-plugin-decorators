0f89322cb9231b2812e6e66bbc7215b88efd02b5
Joseph Bushell <joe@sanpochew.com>
2019-02-27 11:45:19 +0000

    use concat

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const { isFunction, isPlainObject, negate } = require('lodash');
 const importFrom = require('import-from');
 
-const requirePlugin = module => importFrom(process.cwd(), typeof module === 'string' ? module : module[0]);
+const requirePlugin = module => importFrom(process.cwd(), [].concat(module)[0]);
 
 /**
  * @param definition Any valid `semantic-release` plugin format (`String`,
