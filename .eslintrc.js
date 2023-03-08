b011f30a3ac111d6b78542b527288bc4f65d2010
Emanuel Rosal <emanuel.rosal@externos-cl.cencosud.com>
2023-03-08 11:06:09 -0300

    fix: move lib to dependency

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: false,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {},
+};
